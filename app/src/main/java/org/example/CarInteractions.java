71bc2d178962478f7b1ebb677296ed9f961c7df8
sali223 <sharmarke22@gmail.com>
2024-10-14 17:11:18 -0500

    changed file

---
+++ app/src/main/java/org/example/CarInteractions.java
@@ -0,0 +1,30 @@
+package org.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+// create function to insert into 'cars' table
+
+
+public class CarInteractions {
+    static final String INSERT = "INSERT INTO cars (make) VALUES ('Audi'), ('Range Rover'), ('Land Rover')";
+}
+
+
+
+
+// create function to get by car make
+
+    // create function to get by car model
+    //
+    // public class CarInteractions {
+
+
+
+    // create function to update car by make model year
+
+    // create function to delete car by make model year{
+        // Predefined lists of car makes and models
+
+    //static final String DELETE = "delete from cars";
+
